a380b0974dfab383bb11188aeb063f5318014721
Jan Wilmake <jan@wilmake.com>
2025-01-25 11:26:12 +0100

    improvements

--- types.ts
+++ types.ts
@@ -7,3 +7,13 @@ export type Env2 = {
   GITHUB_STARS_CACHE: KVNamespace;
   CHUNK_PROCESSOR: DurableObjectNamespace;
 };
+
+type Env3 = {
+  GITHUB_STARS_CACHE: KVNamespace;
+  CHUNK_PROCESSOR: DurableObjectNamespace;
+};
+
+interface Env4 {
+  GITHUB_STARS_CACHE: KVNamespace;
+  CHUNK_PROCESSOR: DurableObjectNamespace;
+}
